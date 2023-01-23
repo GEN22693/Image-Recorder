7a9fbb478b41d4235f35fc6621bda8a441846b41
Michael Schletz <schletz@spengergasse.at>
2023-01-23 11:27:45 +0100

    Project scaffolding

---
+++ src/src/App.vue
@@ -0,0 +1,18 @@
+<script setup>
+import ImageRecorder from './components/ImageRecorder.vue';
+</script>
+
+<template>
+    <main>
+        <ImageRecorder></ImageRecorder>
+    </main>
+</template>
+
+<style scoped>
+main {
+    border: 1px solid black;
+    max-width: 1000px;
+    max-height: 562px;
+    margin:auto;
+}
+</style>
---
+++ src/src/components/ImageRecorder.vue
@@ -0,0 +1,30 @@
+<script setup>
+
+</script>
+
+<template>
+    <div><h1>Here you should see a live image from your cam.</h1></div>
+</template>
+
+<style scoped>
+</style>
+
+<script>
+export default {
+    data() {
+        return {
+
+        }
+    },
+    mounted() {
+
+    },
+    methods: {
+
+    },
+    computed: {
+
+    }
+};
+</script>
+
