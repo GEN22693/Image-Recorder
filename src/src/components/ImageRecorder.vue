cafdbbf0da15ba0e3dc093f48e46f338ccde2d65
Stefan Trendafilov <tre22038@spengergasse.at>
2023-01-25 15:22:01 +0100

    ImageRecorder basic function

--- src/src/components/ImageRecorder.vue
+++ src/src/components/ImageRecorder.vue
@@ -1,30 +1,108 @@
-<script setup>
-
-</script>
-
 <template>
-    <div><h1>Here you should see a live image from your cam.</h1></div>
+  <section :style="styling" v-if="isValid" class="photo-capture">
+    <video v-show="showVideo" ref="player" class="camera" autoplay playsinline />
+    <canvas v-show="!showVideo" class="preview" ref="canvas" />
+    <div v-if="!hideBtns" class="center photo-capture-actions" >
+      <button
+        :class="'btn flex-center '  + buttonsClasses"
+        @click.prevent="capture"
+        v-if="showVideo"
+      >{{captureBtnContent}}</button>
+      <div class="controls" v-else>
+        <button :class="'btn '+ buttonsClasses" @click.prevent="cancel">{{cancelBtnContent}}</button>
+        <button :class="'btn '+ buttonsClasses" @click.prevent="done">{{doneBtnContent}}</button>
+      </div>
+    </div>
+  </section>
 </template>
-
-<style scoped>
-</style>
 
 <script>
 export default {
-    data() {
-        return {
-
-        }
+  name: "PhotoCapture",
+  props: {
+    hideBtns:{
+      type:Boolean,
+      isRequired: false,
+      default:false
     },
-    mounted() {
-
+    styling: {
+      type: Object,
+      isRequired: false
     },
-    methods: {
-
+    value: {
+      default: null
     },
-    computed: {
-
+    hideButtons: {
+      type: Boolean,
+      default: false
+    },
+    buttonsClasses: {
+      type: String,
+      default: ""
+    },
+    captureBtnContent: {
+      default: "Capture"
+    },
+    cancelBtnContent: {
+      default: "Cancel"
+    },
+    doneBtnContent: {
+      default: "OK"
     }
+  },
+  data() {
+    return {
+      showVideo: true,
+      picture: null,
+      isValid: true
+    };
+  },
+  mounted() {
+    this.videoPlayer = this.$refs.player;
+    this.canvasElement = this.$refs.canvas;
+    this.streamUserMediaVideo();
+  },
+  methods: {
+    streamUserMediaVideo() {
+      if (!navigator.mediaDevices || !navigator.mediaDevices.getUserMedia) {
+        return;
+      }
+      navigator.mediaDevices
+        .getUserMedia({ video: true })
+        .then(stream => (this.videoPlayer.srcObject = stream))
+        .catch(() => {
+          this.isValid = false;
+        });
+    },
+    capture() {
+      this.showVideo = false;
+      this.canvasElement.width = this.videoPlayer.videoWidth;
+      this.canvasElement.height = this.videoPlayer.videoHeight;
+
+      var context = this.canvasElement.getContext("2d");
+      context.translate(this.canvasElement.width, 0);
+      context.scale(-1, 1);
+
+      context.drawImage(this.$refs.player, 0, 0);
+
+      this.stopVideoStream();
+      this.picture = this.$refs.canvas.toDataURL();
+    },
+    done() {
+      this.$emit("input", this.picture);
+      this.showVideo = true;
+      this.streamUserMediaVideo();
+    },
+    cancel() {
+      this.showVideo = true;
+      this.streamUserMediaVideo();
+    },
+    stopVideoStream() {
+      if (!(this.$refs.player && this.$refs.player.srcObject)) return;
+      this.$refs.player.srcObject.getVideoTracks().forEach(track => {
+        track.stop();
+      });
+    }
+  }
 };
 </script>
-
